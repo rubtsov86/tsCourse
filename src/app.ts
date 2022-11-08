bce03f553cb645bd2df58a3871ef813e3b8df856
Serhii Rubtsov <rubtsov1986@bigmir.net>
2022-11-08 21:06:19 +0200

    finish 5 task

--- src/app.ts
+++ src/app.ts
@@ -1,64 +1,115 @@
-//task 4
+//tasr 5
 
 {
-  class Key {
-    private signature: number;
-
-    constructor() {
-      this.signature = Math.random();
-    }
-
-    getSignature(): number {
-      return this.signature;
-    }
+  function getPromise(): Promise<Array<string | number>> {
+    return new Promise(resolve => {
+      resolve(['Text', 50]);
+    });
   }
 
-  class Person {
-    private key: Key;
-
-    constructor(k: Key) {
-      this.key = k;
-    }
-
-    getKey(): Key {
-      return this.key;
-    }
-  }
-
-  abstract class House {
-    protected door = false;
-    protected tenants: Person[] = [];
-    constructor(protected key: Key) {}
-
-    comeIn(person: Person): void {
-      if (this.door) {
-        this.tenants.push(person);
-      } else {
-        throw new Error('Door is close');
-      }
-    }
-
-    abstract openDoor(k: Key): boolean;
-  }
-
-  class MyHouse extends House {
-    openDoor(key: Key) {
-      if (key.getSignature() !== this.key.getSignature()) {
-        throw new Error('Key to another door');
-      }
-      return (this.door = true);
-    }
-  }
-
-  const key = new Key();
-
-  const house = new MyHouse(key);
-  const person = new Person(key);
-
-  house.openDoor(person.getKey());
-
-  house.comeIn(person);
+  getPromise().then(data => {
+    console.log(data);
+  });
 }
+
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
+
+function compare(
+  top: Pick<AllType, 'name' | 'color'>,
+  bottom: Pick<AllType, 'position' | 'weight'>
+): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+class Component<T> {
+  constructor(public props: T) {}
+}
+
+interface IProps {
+  title: string;
+}
+
+class Page extends Component<IProps> {
+  pageInfo() {
+    console.log(this.props.title);
+  }
+}
+
+//task 4
+
+// {
+//   class Key {
+//     private signature: number;
+
+//     constructor() {
+//       this.signature = Math.random();
+//     }
+
+//     getSignature(): number {
+//       return this.signature;
+//     }
+//   }
+
+//   class Person {
+//     private key: Key;
+
+//     constructor(k: Key) {
+//       this.key = k;
+//     }
+
+//     getKey(): Key {
+//       return this.key;
+//     }
+//   }
+
+//   abstract class House {
+//     protected door = false;
+//     protected tenants: Person[] = [];
+//     constructor(protected key: Key) {}
+
+//     comeIn(person: Person): void {
+//       if (this.door) {
+//         this.tenants.push(person);
+//       } else {
+//         throw new Error('Door is close');
+//       }
+//     }
+
+//     abstract openDoor(k: Key): boolean;
+//   }
+
+//   class MyHouse extends House {
+//     openDoor(key: Key) {
+//       if (key.getSignature() !== this.key.getSignature()) {
+//         throw new Error('Key to another door');
+//       }
+//       return (this.door = true);
+//     }
+//   }
+
+//   const key = new Key();
+
+//   const house = new MyHouse(key);
+//   const person = new Person(key);
+
+//   house.openDoor(person.getKey());
+
+//   house.comeIn(person);
+// }
 
 // task1
 
